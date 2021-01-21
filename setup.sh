ddc44eb91ea6ae9f87fd18236baad23763a78866
Kam Lasater <ckl@seekayel.com>
2021-01-21 05:07:43 -0500

    Trimmed up messaging

--- setup.sh
+++ setup.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
 
-set -ux
-
 echo 'Starting setup.sh'
 
-# aws iam create-service-specific-credential --user-name (aws iam get-user | jq -r '.User.UserName') --service-name codecommit.amazonaws.com
-
-# git remote add aws-deploy https://git-codecommit.us-east-2.amazonaws.com/v1/repos/$REPO_NAME
-
-ls -lart ~/.ssh/
-
+#
+## Generate SSH keys
+#
 if [ ! -f ~/.ssh/id_rsa ]; then
     echo 'Generating public/private ssh keys'
     ssh-keygen -f '~/.ssh/id_rsa' -q -t rsa -b 4096 -C "Cyclic generated ssh public key for codecommit"
 fi
 
-# eval "$(ssh-agent -s)"
-
-# cat >> ~/.ssh/config <<'EOF'
-# Host *
-#  AddKeysToAgent yes
-#  UseKeychain yes
-#  IdentityFile ~/.ssh/id_rsa
-# EOF
-
-# ssh-add -K ~/.ssh/id_rsa
-
+#
+## Upload public key and config if not already configured.
+#
 grep -q 'git-codecommit.*.amazonaws.com' ~/.ssh/config
 if [[ $? > 0 ]]; then
     echo 'No codecommit ssh key configured. Uploading.'
@@ -47,6 +34,11 @@ EOF
     chmod 600 ~/.ssh/config
 fi
 
+#
+## Success and next steps.
+#
 echo 'You are now be configured. To test your ssh config you can run:'
 echo ''
 echo ' ssh git-codecommit.$AWS_DEFAULT_REGION.amazonaws.com'
+
+# git remote add aws-deploy https://git-codecommit.us-east-2.amazonaws.com/v1/repos/$REPO_NAME

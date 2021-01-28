ba0ab49b34eb0945db7d9c0d2db47cf6ad54478c
Kam Lasater <kam@cyclic.sh>
2021-01-28 06:32:26 -0500

    Pre-commit added and cleaned up scripts

--- account/bootstrap.sh
+++ account/bootstrap.sh
@@ -5,12 +5,12 @@ aws --version
 
 temp_template=$(date +bootstrap-%Y%m%d-%H%M.yaml)
 
-curl -s https://raw.githubusercontent.com/cyclic-software/setup/main/account/bootstrap.yaml > $temp_template
+curl -s https://raw.githubusercontent.com/cyclic-software/setup/main/account/bootstrap.yaml >$temp_template
 
 aws cloudformation create-stack \
-    --stack-name CyclicBootstrapStack \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --template-body file://$temp_template
+	--stack-name CyclicBootstrapStack \
+	--capabilities CAPABILITY_NAMED_IAM \
+	--template-body file://$temp_template
 
 rm $temp_template
 
--- setup.sh
+++ setup.sh
@@ -5,22 +5,20 @@ echo 'Starting setup.sh'
 #
 ## Install git-comit-remote auth git plugin
 #
-if command -v pip3 &> /dev/null
-then
-    pip3 install --quiet --no-input git-remote-codecommit
-elif command -v pip &> /dev/null
-then
-    pip install --quiet --no-input  git-remote-codecommit
+if command -v pip3 &>/dev/null; then
+	pip3 install --quiet --no-input git-remote-codecommit
+elif command -v pip &>/dev/null; then
+	pip install --quiet --no-input git-remote-codecommit
 else
-    echo "nothing matched"
+	echo "nothing matched"
 fi
 
 #
 ## Generate SSH keys
 #
 if [ ! -f ~/.ssh/id_rsa ]; then
-    echo 'Generating public/private ssh keys'
-    ssh-keygen -f ~/.ssh/id_rsa -q -t rsa -b 4096 -C "Cyclic generated ssh public key for codecommit"
+	echo 'Generating public/private ssh keys'
+	ssh-keygen -f ~/.ssh/id_rsa -q -t rsa -b 4096 -C "Cyclic generated ssh public key for codecommit"
 fi
 
 #
@@ -28,23 +26,24 @@ fi
 #
 grep -q 'git-codecommit.*.amazonaws.com' ~/.ssh/config
 if [[ $? > 0 ]]; then
-    echo 'No codecommit ssh key configured. Uploading.'
+	echo 'No codecommit ssh key configured. Uploading.'
 
-    key_id=$(
-    aws iam upload-ssh-public-key \
-        --user-name $(aws iam get-user | jq -r '.User.UserName') \
-        --ssh-public-key-body file://~/.ssh/id_rsa.pub \
-    | jq -r '.SSHPublicKey.SSHPublicKeyId')
+	key_id=$(
+		aws iam upload-ssh-public-key \
+			--user-name $(aws iam get-user | jq -r '.User.UserName') \
+			--ssh-public-key-body file://~/.ssh/id_rsa.pub |
+			jq -r '.SSHPublicKey.SSHPublicKeyId'
+	)
 
-    # Prepend the gitcommit ssh key info to
-    cp ~/.ssh/config ~/.ssh/config_pre_cyclic_install_$(date +%Y%m%d)
-cat > ~/.ssh/config << EOF
+	# Prepend the gitcommit ssh key info to
+	cp ~/.ssh/config ~/.ssh/config_pre_cyclic_install_$(date +%Y%m%d)
+	cat >~/.ssh/config <<EOF
 Host git-codecommit.*.amazonaws.com
   User $key_id
   IdentityFile ~/.ssh/id_rsa
 EOF
-    cat ~/.ssh/config_pre_cyclic_install_$(date +%Y%m%d) >> ~/.ssh/config
-    chmod 600 ~/.ssh/config
+	cat ~/.ssh/config_pre_cyclic_install_$(date +%Y%m%d) >>~/.ssh/config
+	chmod 600 ~/.ssh/config
 fi
 
 #
@@ -57,7 +56,7 @@ PROFILE="\$AWS_PROFILE_NAME"
 #   PROFILE="$AWS_PROFILE"
 # fi
 
-cat << EOF
+cat <<EOF
 You are now be configured. To test your ssh config run:
 
  % ssh git-codecommit.$REGION.amazonaws.com
@@ -74,7 +73,6 @@ You can now add git remotes with AWS profiles (from ~/.aws/credentials):
 
 
 EOF
-
 
 # git remote add cyclic https://git-codecommit.$REGION.amazonaws.com/v1/repos/$REPO_NAME
 # git remote add cyclic codecommit::$AWS_DEFAULT_REGION://$AWS_PROFILE_NAME@$REPO_NAME
